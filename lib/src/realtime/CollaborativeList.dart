adeff78ed30f67c100a0d898fb8fb24597cbbbdb
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2013-04-26 22:23:15 +0200

    add generics to CollaborativeList

--- lib/src/realtime/CollaborativeList.dart
+++ lib/src/realtime/CollaborativeList.dart
@@ -14,61 +14,68 @@
 
 part of google_drive_realtime;
 
-class CollaborativeList extends CollaborativeObject {
-  static CollaborativeList cast(js.Proxy proxy) => proxy == null ? null : new CollaborativeList.fromProxy(proxy);
+class CollaborativeList<E> extends CollaborativeObject {
+  static CollaborativeList cast(js.Proxy proxy, [jsw.Translator translator]) => proxy == null ? null : new CollaborativeList.fromProxy(proxy, translator);
+
+  static CollaborativeList castListOfSerializables(js.Proxy proxy, jsw.Mapper<dynamic, js.Serializable> fromJs, {mapOnlyNotNull: false}) => proxy == null ? null : new CollaborativeList.fromProxy(proxy, new jsw.TranslatorForSerializable(fromJs, mapOnlyNotNull: mapOnlyNotNull));
+
+  final jsw.Translator<E> _translator;
 
   Stream<ValuesAddedEvent> _onValuesAdded;
   Stream<ValuesRemovedEvent> _onValuesRemoved;
   Stream<ValuesSetEvent> _onValuesSet;
 
-  CollaborativeList.fromProxy(js.Proxy proxy) : super.fromProxy(proxy) {
+  CollaborativeList.fromProxy(js.Proxy proxy, [jsw.Translator<E> translator]) : this._translator = translator, super.fromProxy(proxy) {
     _onValuesAdded = _getStreamFor(EventType.VALUES_ADDED, ValuesAddedEvent.cast);
     _onValuesRemoved = _getStreamFor(EventType.VALUES_REMOVED, ValuesRemovedEvent.cast);
     _onValuesSet = _getStreamFor(EventType.VALUES_SET, ValuesSetEvent.cast);
   }
 
+  dynamic _toJs(E e) => _translator == null ? e : _translator.toJs(e);
+  E _fromJs(dynamic value) => _translator == null ? value : _translator.fromJs(value);
+
   int get length => $unsafe['length'];
 
   void clear() { $unsafe.clear(); }
-  dynamic get(int index) => $unsafe.get(index);
-  void insert(int index, dynamic value) { $unsafe.insert(index, value); }
-  int push(dynamic value) => $unsafe.push(value);
+  E get(int index) => _fromJs($unsafe.get(index));
+  void insert(int index, E value) { $unsafe.insert(index, _toJs(value)); }
+  int push(E value) => $unsafe.push(_toJs(value));
   IndexReference registerReference(int index, bool canBeDeleted) => IndexReference.cast($unsafe.registerReference(index, canBeDeleted));
   void remove(int index) { $unsafe.remove(index); }
   void removeRange(int startIndex, int endIndex) { $unsafe.removeRange(startIndex, endIndex); }
-  bool removeValue(dynamic value) => $unsafe.removeValue(value);
-  void set(int index, dynamic value) { $unsafe.set(index, value); }
+  bool removeValue(E value) => $unsafe.removeValue(_toJs(value));
+  void set(int index, E value) { $unsafe.set(index, _toJs(value)); }
 
-  List asArray() => jsw.JsArrayToListAdapter.cast($unsafe.asArray());
-  int indexOf(dynamic value, [Comparator comparator]) {
+  List<E> asArray() => jsw.JsArrayToListAdapter.cast($unsafe.asArray(), _translator);
+  int indexOf(E value, [Comparator comparator]) {
     js.Callback comparatorCallback = null;
     if (comparator != null) {
       comparatorCallback = new js.Callback.many(comparator);
     }
     try {
-      return $unsafe.indexOf(value, comparatorCallback);
+      return $unsafe.indexOf(_toJs(value), comparatorCallback);
     } finally {
       if (comparatorCallback != null) {
         comparatorCallback.dispose();
       }
     }
   }
-  void insertAll(int index, List values) { $unsafe.insertAll(index, values is js.Serializable<js.Proxy> ? values : js.array(values)); }
-  int lastIndexOf(dynamic value, [Comparator comparator]) {
+  void insertAll(int index, List<E> values) { $unsafe.insertAll(index, values is js.Serializable<js.Proxy> ? values : js.array(values.map(_toJs))); }
+  int lastIndexOf(E value, [Comparator comparator]) {
     js.Callback comparatorCallback = null;
     if (comparator != null) {
       comparatorCallback = new js.Callback.many(comparator);
     }
     try {
-      return $unsafe.lastIndexOf(value, comparatorCallback);
+      return $unsafe.lastIndexOf(_toJs(value), comparatorCallback);
     } finally {
       if (comparatorCallback != null) {
         comparatorCallback.dispose();
       }
     }
   }
-  void pushAll(List values) { $unsafe.pushAll(values is js.Serializable<js.Proxy> ? values : js.array(values)); }
-  void replaceRange(int index, List values) { $unsafe.replaceRange(index, values is js.Serializable<js.Proxy> ? values : js.array(values)); }
+  void pushAll(List<E> values) { $unsafe.pushAll(values is js.Serializable<js.Proxy> ? values : js.array(values.map(_toJs))); }
+  void replaceRange(int index, List<E> values) { $unsafe.replaceRange(index, values is js.Serializable<js.Proxy> ? values : js.array(values.map(_toJs))); }
 
   Stream<ValuesAddedEvent> get onValuesAdded => _onValuesAdded;
   Stream<ValuesRemovedEvent> get onValuesRemoved => _onValuesRemoved;
