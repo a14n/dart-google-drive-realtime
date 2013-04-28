a301c9fe311babfd2fd04ff4096b4c6cfe57bc43
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2013-04-28 22:03:20 +0200

    add some unittests

---
+++ test/browser_tests.dart
@@ -0,0 +1,187 @@
+import 'dart:html';
+import 'dart:async';
+
+import 'package:js/js.dart' as js;
+import 'package:js/js_wrapping.dart' as jsw;
+import 'package:google_drive_realtime/google_drive_realtime.dart' as rt;
+import 'package:google_drive_realtime/google_drive_realtime_custom.dart' as rtc;
+import 'package:unittest/unittest.dart';
+import 'package:unittest/html_config.dart';
+
+initializeModel(js.Proxy modelProxy) {
+  var model = rt.Model.cast(modelProxy);
+  model.root['text'] = model.createString('Hello Realtime World!');
+  model.root['list'] = model.createList();
+  model.root['map'] = model.createMap();
+}
+
+onFileLoaded(docProxy) {
+  var doc = rt.Document.cast(docProxy);
+  js.retain(doc);
+
+  useHtmlConfiguration();
+
+  group('CollaborativeString', () {
+    var string = rt.CollaborativeString.cast(doc.model.root['text']);
+    js.retain(string);
+    setUp((){
+      string.text = 'unittest';
+    });
+    test('get length', () {
+      expect(string.length, 8);
+    });
+    test('append(String text)', () {
+      string.append(' append');
+      expect(string.text, 'unittest append');
+    });
+    test('get text', () {
+      expect(string.text, 'unittest');
+    });
+    test('insertString(int index, String text)', () {
+      string.insertString(4, ' append ');
+      expect(string.text, 'unit append test');
+    });
+    test('removeRange(int startIndex, int endIndex)', () {
+      string.removeRange(4, 6);
+      expect(string.text, 'unitst');
+    });
+    test('set text(String text)', () {
+      string.text = 'newValue';
+      expect(string.text, 'newValue');
+    });
+    test('onTextInserted', () {
+      StreamSubscription ssInsert;
+      StreamSubscription ssDelete;
+      ssInsert = string.onTextInserted.listen(expectAsync1((rt.TextInsertedEvent e) {
+        expect(e.index, 4);
+        expect(e.text, ' append ');
+        ssInsert.cancel();
+        ssDelete.cancel();
+      }));
+      ssDelete = string.onTextDeleted.listen(expectAsync1((rt.TextDeletedEvent e) {
+        fail("delete should not be call");
+      }, count: 0));
+      string.insertString(4, ' append ');
+    });
+    test('onTextDeleted', () {
+      StreamSubscription ssInsert;
+      StreamSubscription ssDelete;
+      ssInsert = string.onTextInserted.listen(expectAsync1((rt.TextInsertedEvent e) {
+        fail("insert should not be call");
+      }, count: 0));
+      ssDelete = string.onTextDeleted.listen(expectAsync1((rt.TextDeletedEvent e) {
+        expect(e.index, 4);
+        expect(e.text, 'te');
+        ssInsert.cancel();
+        ssDelete.cancel();
+      }));
+      string.removeRange(4, 6);
+    });
+  });
+
+  group('CollaborativeList', () {
+    var list = rt.CollaborativeList.cast(doc.model.root['list']);
+    js.retain(list);
+    setUp((){
+      list.clear();
+      list.push('s1');
+    });
+    test('get length', () {
+      expect(list.length, 1);
+    });
+    test('operator [](int index)', () {
+      expect(list[0], 's1');
+      expect(() => list[-1], throws);
+      expect(() => list[1], throws);
+    });
+    test('operator []=(int index, E value)', () {
+      list[0] = 'new s1';
+      expect(list[0], 'new s1');
+    });
+    test('clear()', () {
+      list.clear();
+      expect(list.length, 0);
+    });
+    test('insert(int index, E value)', () {
+      list.insert(0, 's0');
+      expect(list.length, 2);
+      expect(list[0], 's0');
+      expect(list[1], 's1');
+    });
+    test('push(E value)', () {
+      expect(list.push('s2'), 2);
+      expect(list.length, 2);
+      expect(list[0], 's1');
+      expect(list[1], 's2');
+    });
+    test('onValuesAdded', () {
+      StreamSubscription ss;
+      ss = list.onValuesAdded.listen(expectAsync1((rt.ValuesAddedEvent e) {
+        expect(e.index, 1);
+        expect(e.values, ['s2']);
+        ss.cancel();
+      }));
+      list.push('s2');
+    });
+    test('onValuesRemoved', () {
+      StreamSubscription ss;
+      ss = list.onValuesRemoved.listen(expectAsync1((rt.ValuesRemovedEvent e) {
+        expect(e.index, 0);
+        expect(e.values, ['s1']);
+        ss.cancel();
+      }));
+      list.clear();
+    });
+    test('onValuesSet', () {
+      StreamSubscription ss;
+      ss = list.onValuesSet.listen(expectAsync1((rt.ValuesSetEvent e) {
+        expect(e.index, 0);
+        expect(e.oldValues, ['s1']);
+        expect(e.newValues, ['s2']);
+        ss.cancel();
+      }));
+      list[0] = 's2';
+    });
+  });
+}
+
+/**
+ * Options for the Realtime loader.
+ */
+get realtimeOptions => js.map({
+   /**
+  * Client ID from the APIs Console.
+  */
+  'clientId': 'INSERT YOUR CLIENT ID HERE',
+
+   /**
+  * The ID of the button to click to authorize. Must be a DOM element ID.
+  */
+   'authButtonElementId': 'authorizeButton',
+
+   /**
+  * Function to be called when a Realtime model is first created.
+  */
+   'initializeModel': new js.Callback.once(initializeModel),
+
+   /**
+  * Autocreate files right after auth automatically.
+  */
+   'autoCreate': true,
+
+   /**
+  * Autocreate files right after auth automatically.
+  */
+   'defaultTitle': "New Realtime Quickstart File",
+
+   /**
+  * Function to be called every time a Realtime file is loaded.
+  */
+   'onFileLoaded': new js.Callback.many(onFileLoaded)
+});
+
+
+main() {
+  var realtimeLoader = new js.Proxy(js.context.rtclient.RealtimeLoader, realtimeOptions);
+  realtimeLoader.start();
+}
