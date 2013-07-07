15a5817f3175ad05c908569330d6fe98839a9668
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2013-07-07 23:50:14 +0200

    workaround issue 95 in js-interop to make unsubscription work (point 2 in #4)

--- lib/src/realtime/EventTarget.dart
+++ lib/src/realtime/EventTarget.dart
@@ -24,15 +24,22 @@ class EventTarget extends jsw.TypedProxy {
 
   SubscribeStreamProvider _getStreamProviderFor(EventType eventType, [transformEvent(e)]) {
     js.Callback handler;
+    // TODO remove jsFunction when https://github.com/dart-lang/js-interop/issues/95 is fixed
+    js.FunctionProxy jsFunction;
     return new SubscribeStreamProvider(
         subscribe: (EventSink eventSink) {
           handler = new js.Callback.many((e) {
             eventSink.add(transformEvent == null ? e : transformEvent(e));
           });
-          _addEventListener(eventType, handler);
+          js.context['hackForJsInterop95'] = handler;
+          jsFunction = js.context['hackForJsInterop95'];
+          js.retain(jsFunction);
+          js.deleteProperty(js.context, "hackForJsInterop95");
+          _addEventListener(eventType, /*handler */ jsFunction);
         },
         unsubscribe: (EventSink eventSink) {
-          _removeEventListener(eventType, handler);
+          _removeEventListener(eventType, /*handler */ jsFunction);
+          js.release(jsFunction);
           handler.dispose();
         }
     );
